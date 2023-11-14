a6d7866b1daceaa44310158462bc24864f51cf93
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-11-14 19:43:31 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/RRomeo-RJ/Romeo-musicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
