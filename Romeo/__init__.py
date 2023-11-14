13e9fe85f595b64562f1be9ee36f79b22bf6842f
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2023-11-14 18:42:41 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

---
+++ Romeo/__init__.py
@@ -0,0 +1,38 @@
+from Romeo.core.bot import RjBot
+from Romeo.core.dir import dirr
+from Romeo.core.git import git
+from Romeo.core.userbot import Userbot
+from Romeo.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = RjBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
